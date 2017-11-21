da5349159eb0705e8fe660de853e5a2b361245fc
artyom <art231188@gmail.com>
2017-11-21 11:38:45 +0200

    fix task#12, attempt 2

--- addressbook-web-tests/addressbook-web-tests/tests/ContactDetailsTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/ContactDetailsTests.cs
@@ -10,17 +10,13 @@ namespace WebAddressBookTests
         [Test]
         public void CompareDetailsAndEditPagesTest()
         {
-            ContactData fromEditPage = app.contacts.GetContactInformationFromEditForm(0);
-            ContactData fromDetailPage = app.contacts.GetContactInformationFromDetailsPage(0);
+            ContactData fromEditPage = app.contacts.GetContactInformationFromEditForm(2);
+            ContactData fromDetailPage = app.contacts.GetContactInformationFromDetailsPage(2);
 
             Assert.AreEqual(fromEditPage.AllUserInfo, fromDetailPage.AllUserInfo);
         }
 
-        [Test]
-        public void GetContdetails()
-        {
-            ContactData fromDetails = app.contacts.GetContactInformationFromDetailsPage(0);
-            System.Console.WriteLine(fromDetails.AllUserInfo);
-        }
+
     }
 }
+
